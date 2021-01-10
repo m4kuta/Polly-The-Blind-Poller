cb9925522920b15cc922b4aab5fff63e92b6d01f
fanhaoli <haolin.fan@mail.utoronto.ca>
2021-01-09 21:03:41 -0500

    Work on Poll

--- Poll.py
+++ Poll.py
@@ -1,8 +1,19 @@
 class Poll:
-    def __init__(self, question, options):
-        # self.id =
-        # self.creator =
-        self.question = question
-        # self.votes =
-        
+    def __init__(self, creator, question, options):
+        self._id = id
+        self._creator = creator
+        self._question = question
+        self._votes = {}
+
+    def get_id(self):
+        return self._id
+
+    def get_creator(self):
+        return self._creator
+
+    def get_question(self):
+        return self._question
+
+    def get_votes(self):
+        return self._votes
 
