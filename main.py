c9fde4831658d6aac4e0b7aaf64c74a3275e5b3f
Frank Long <fclong@uwaterloo.ca>
2021-01-10 13:41:11 -0500

    Add poll timer

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
+import datetime
 from discord.ext import commands
 from config import token
 from PollManager import PollManager
+import asyncio
 my_token = token
 emojiLetters = [
             "\N{REGIONAL INDICATOR SYMBOL LETTER A}",
@@ -41,23 +43,28 @@ async def on_ready():
 @client.command()
 async def poll(ctx, question, *args):
 	voters = []
-	activePoll = True
+	vote_counts = {}
 	
 	# Create poll
 	options = []
+	react_to_option = {}
 	description = ""
 	for i, arg in enumerate(args):
 		description += emojiLetters[i] + " " + arg + "\n"
 		options.append(arg)
-
+		react_to_option[emojiLetters[i]] = arg
+	print(react_to_option)
+	# Initialize vote_counts dictionary
+	for option in options:
+		vote_counts[option] = 0
+	print(vote_counts)
 	my_poll = discord.Embed(
 		title = question,
 		description = description
 		)
-	message = await ctx.send(embed = my_poll)
-	print(ctx.author)
 	creator = ctx.author
-	await creator.send(embed = my_poll)
+	message = await ctx.send(embed = my_poll)
+	start_time = datetime.datetime.now()
 
 	for i, option in enumerate(options):
 		await message.add_reaction(emojiLetters[i])
@@ -69,16 +76,29 @@ async def poll(ctx, question, *args):
 	def check(reaction, user):
 		return reaction.message.id == message.id and user.id != 797601108268155001 
 
-	while activePoll:
-		reaction, user = await client.wait_for('reaction_add', check = check)
-		await message.remove_reaction(reaction, user)
-		# Check if the user has already voted
-		if user not in voters:
-			voters.append(user)
-			user_name = user.display_name
-			await creator.send(user_name + " voted for " + str(reaction))
-
+	while True: # Exit after a certain time
+		try:
+			reaction, user = await client.wait_for('reaction_add', timeout = 5.0, check = check)
+			await message.remove_reaction(reaction, user)
+			# Check if the user has already voted
+			if user not in voters:
+				voters.append(user)
+				vote_counts[react_to_option[reaction.emoji]] += 1
+				print(vote_counts)
+		except asyncio.TimeoutError:
+			if datetime.datetime.now() > start_time + datetime.timedelta(seconds = 15):
+				break		
+		
+	# Send messages of results 
 	print("done")
-
+	results = ""
+	for option in vote_counts:
+		results += option + ": " + str(vote_counts[option]) + "\n"
+	results_message = discord.Embed(
+		title = question,
+		description = results
+		)
+	await message.clear_reactions()
+	await ctx.send(embed = results_message)
 
 client.run(my_token)
